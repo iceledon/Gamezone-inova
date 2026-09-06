c227bb32b17e3628e2cec5ed27705adc17b99056
Jaime Granados <jaimelcartero99@gmail.com>
2026-09-06 17:06:51 -0500

    fix: restore JavaDoc descriptions removed by mistake

--- src/main/java/com/gamezone/model/Product.java
+++ src/main/java/com/gamezone/model/Product.java
@@ -1,6 +1,9 @@
 package com.gamezone.model;
 
-
+/**
+ * Represents a generic product sold by the store.
+ * Abstract because every product sold is always a specific type.
+ */
 public abstract class Product {
 
     private String id;
@@ -9,12 +12,12 @@ public abstract class Product {
     private int quantity;
 
     /**
-     * 
+     * Creates a new product with its basic information.
      *
-     * @param id       
-     * @param title    
-     * @param price    
-     * @param quantity 
+     * @param id       unique identifier of the product
+     * @param title    name of the product
+     * @param price    unit price of the product
+     * @param quantity units currently available in inventory
      */
     public Product(String id, String title, double price, int quantity) {
         this.id = id;
@@ -24,56 +27,56 @@ public abstract class Product {
     }
 
     /**
-     * @return 
+     * @return the unique identifier of the product
      */
     public String getId() {
         return id;
     }
 
     /**
-     * @param id 
+     * @param id the new unique identifier of the product
      */
     public void setId(String id) {
         this.id = id;
     }
 
     /**
-     * @return 
+     * @return the name of the product
      */
     public String getTitle() {
         return title;
     }
 
     /**
-     * @param title 
+     * @param title the new name of the product
      */
     public void setTitle(String title) {
         this.title = title;
     }
 
     /**
-     * @return 
+     * @return the unit price of the product
      */
     public double getPrice() {
         return price;
     }
 
     /**
-     * @param price 
+     * @param price the new unit price of the product
      */
     public void setPrice(double price) {
         this.price = price;
     }
 
     /**
-     * @return 
+     * @return the units currently available in inventory
      */
     public int getQuantity() {
         return quantity;
     }
 
     /**
-     * @param quantity 
+     * @param quantity the new quantity available in inventory
      */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
